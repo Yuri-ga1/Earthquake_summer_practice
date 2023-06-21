e8c4d011f63002f40c1d9a571d5e4fc9c381fad8
Yuri-ga1 <regota09@gmail.com>
2023-06-21 22:19:21 +0800

    create folder for user data

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from pydantic import EmailStr, BaseModel
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import os
+from datetime import datetime as dt
 
 api = FastAPI()
 
@@ -7,33 +10,36 @@ api = FastAPI()
 @api.get("/")
 async def index():
     return {"Message": "Hello everyone"}
-
-@api.get("/hello")
-async def hello(user_name: str | None = None):
-    if user_name:
-        return {"Message": f"Hello {user_name}"}
-    else:
-        return {"Message": "Hello anonymous"}
     
 @api.post("/create_user")
 async def create_user(user_name: str,
                       user_mail: EmailStr):
+    os.mkdir(f"users/{EmailStr}")
     return {"user_name": user_name,
             "user_mail": user_mail}
 
-
-class UserBase(BaseModel):
-    name: str
-    email: EmailStr
-
-class UserOut(UserBase):
-    pass
-
-class UserIn(UserBase):
-    password: str
+@api.post("/upload_files")
+async def upload_files(user_mail: EmailStr, user_data):
+    if "if user doesn't exist":
+        return RedirectResponse('/create_user')
+    else:
+        today = str(dt.now())[:-7].split()
+        folder_name = today[0] + "_" + today[1]
+        os.mkdir(f"users/{EmailStr}/{folder_name}")
 
 
-@api.post("/create_user_from_model", response_model=UserOut)
-async def create_user_from_model(user: UserIn):
-    return user
 
+#class UserBase(BaseModel):
+#    name: str
+#    email: EmailStr
+#
+#class UserOut(UserBase):
+#    pass
+#
+#class UserIn(UserBase):
+#    password: str
+#
+#
+#@api.post("/create_user_from_model", response_model=UserOut)
+#async def create_user_from_model(user: UserIn):
+#    return user
