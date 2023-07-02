9a944075c1b69ea1875e3d71b45d47c8cc605ca3
rassAV <126838987+rassAV@users.noreply.github.com>
2023-07-03 05:08:33 +0800

    Add new tests and corrected old tests

--- app/test_crud.py
+++ app/test_crud.py
@@ -37,10 +37,11 @@ def test_create_path_db(test_db, caplog):
     caplog.set_level("INFO")
 
     email = "test@example.com"
-    path = "/test/path"
+    path = "/users/test@example.com/2023-07-03/roti_01_17.h5"
     date_upload = "2023-06-27"
     date_eq_start = "2023-06-01"
     date_eq_end = "2023-06-30"
+    filename = "roti_01_17.h5"
 
     user_data = UserIn(email="test@example.com", password="password")
     user = create_user_db(test_db, user_data)
@@ -51,14 +52,37 @@ def test_create_path_db(test_db, caplog):
         date_upload=date_upload,
         date_eq_start=date_eq_start,
         date_eq_end=date_eq_end,
+        filename=filename
     )
 
     assert created_path is not None
     assert created_path.email == "test@example.com"
-    assert created_path.path == "/test/path"
+    assert created_path.path == "/users/test@example.com/2023-07-03/roti_01_17.h5"
     assert created_path.date_upload == "2023-06-27"
     assert created_path.date_eq_start == "2023-06-01"
     assert created_path.date_eq_end == "2023-06-30"
+    assert created_path.filename == "roti_01_17.h5"
+
+def test_create_result_db(test_db, caplog):
+    caplog.set_level("INFO")
+
+    email = "test@example.com"
+    path = "/users/test@example.com/2023-07-03/roti_01_17.h5"
+    filename = "roti_01_17.h5"
+
+    user_data = UserIn(email="test@example.com", password="password")
+    user = create_user_db(test_db, user_data)
+    created_result = create_result_db(
+        db=test_db,
+        email=email,
+        filename=filename,
+        path=path
+    )
+
+    assert created_result is not None
+    assert created_result.email == "test@example.com"
+    assert created_result.path == "/users/test@example.com/2023-07-03/roti_01_17.h5"
+    assert created_result.filename == "roti_01_17.h5"
 
 def test_get_user_by_email(test_db, caplog):
     caplog.set_level("INFO")
@@ -76,11 +100,11 @@ def test_get_path(test_db, caplog):
     caplog.set_level("INFO")
      
     email = "test@example.com"
-    path = "/test/path"
+    path = "/users/test@example.com/2023-07-03/roti_01_17.h5"
     date_upload = "2023-06-27"
     date_eq_start = "2023-06-01"
     date_eq_end = "2023-06-30"
-
+    filename = "roti_01_17.h5"
     
     user_data = UserIn(email="test@example.com", password="password")
     user = create_user_db(test_db, user_data)
@@ -91,27 +115,30 @@ def test_get_path(test_db, caplog):
         date_upload=date_upload,
         date_eq_start=date_eq_start,
         date_eq_end=date_eq_end,
+        filename=filename
     )
 
-    retrieved_path = get_path(test_db, path="/test/path")
-
+    retrieved_path = get_path(
+        db=test_db,
+        email=email,
+        filename=filename
+    )
+    
     assert retrieved_path is not None
     assert retrieved_path.email == "test@example.com"
-    assert retrieved_path.path == "/test/path"
-    assert retrieved_path.date_upload == "2023-06-27"
-    assert retrieved_path.date_eq_start == "2023-06-01"
-    assert retrieved_path.date_eq_end == "2023-06-30"
+    assert retrieved_path.filename == "roti_01_17.h5"
 
 def test_get_last_data(test_db, caplog):
     caplog.set_level("INFO")
     
     email = "test@example.com"
-    path1 = "/test/path1"
-    path2 = "/test/path2"
+    path1 = "/users/test@example.com/2023-07-03/roti_01_17.h5"
+    path2 = "/users/test@example.com/2009-07-03/roti_01_17.h5"
     date_upload1 = "2023-06-27"
     date_upload2 = "2023-06-21"
     date_eq_start = "2023-06-01"
     date_eq_end = "2023-06-30"
+    filename = "roti_01_17.h5"
 
     user_data = UserIn(email="test@example.com", password="password")
     user = create_user_db(test_db, user_data)
@@ -122,6 +149,7 @@ def test_get_last_data(test_db, caplog):
         date_upload=date_upload1,
         date_eq_start=date_eq_start,
         date_eq_end=date_eq_end,
+        filename=filename
     )
     created_path = create_path_db(
         db=test_db,
@@ -130,6 +158,7 @@ def test_get_last_data(test_db, caplog):
         date_upload=date_upload2,
         date_eq_start=date_eq_start,
         date_eq_end=date_eq_end,
+        filename=filename
     )
 
     retrieved_data = get_last_data(test_db, email="test@example.com")
@@ -143,10 +172,11 @@ def test_get_data_by_date(test_db, caplog):
     caplog.set_level("INFO")
     
     email = "test@example.com"
-    path = "/test/path"
+    path = "/users/test@example.com/2023-07-03/roti_01_17.h5"
     date_upload = "2023-06-27"
     date_eq_start = "2023-06-01"
     date_eq_end = "2023-06-30"
+    filename = "roti_01_17.h5"
 
     user_data = UserIn(email="test@example.com", password="password")
     user = create_user_db(test_db, user_data)
@@ -157,6 +187,7 @@ def test_get_data_by_date(test_db, caplog):
         date_upload=date_upload,
         date_eq_start=date_eq_start,
         date_eq_end=date_eq_end,
+        filename=filename
     )
 
     retrieved_data = get_data_by_date(test_db, email="test@example.com", date="2023-06-01")
@@ -165,3 +196,53 @@ def test_get_data_by_date(test_db, caplog):
     assert len(retrieved_data) > 0
     assert retrieved_data[0].email == "test@example.com"
     assert retrieved_data[0].date_eq_start == "2023-06-01"
+
+def test_get_results_db(test_db, caplog):
+    caplog.set_level("INFO")
+
+    email = "test@example.com"
+    path = "/users/test@example.com/2023-07-03/roti_01_17.h5"
+    filename = "roti_01_17.h5"
+
+    user_data = UserIn(email="test@example.com", password="password")
+    user = create_user_db(test_db, user_data)
+    created_result = create_result_db(
+        db=test_db,
+        email=email,
+        filename=filename,
+        path=path
+    )
+
+    results = get_results_db(
+        db=test_db,
+        email=email
+    )
+
+    assert results is not None
+    assert results[0].email == "test@example.com"
+
+def test_get_results_db_by_path(test_db, caplog):
+    caplog.set_level("INFO")
+
+    email = "test@example.com"
+    path = "/users/test@example.com/2023-07-03/roti_01_17.h5"
+    filename = "roti_01_17.h5"
+
+    user_data = UserIn(email="test@example.com", password="password")
+    user = create_user_db(test_db, user_data)
+    created_result = create_result_db(
+        db=test_db,
+        email=email,
+        filename=filename,
+        path=path
+    )
+
+    results = get_result_db_by_path(
+        db=test_db,
+        email=email,
+        path=path
+    )
+
+    assert results is not None
+    assert results.email == "test@example.com"
+    assert results.path == "/users/test@example.com/2023-07-03/roti_01_17.h5"
