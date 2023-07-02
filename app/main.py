ab8ef69ae4a2a377394a54591bea224bd25c7ce8
NikitaBagulov <nikita.bagulov.arshan@gmail.com>
2023-07-02 11:48:05 +0800

    add create_plot_dt

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import io
 import os.path
 import sys
 import uvicorn
-from earthquake.app.earthquake import plot_map, retrieve_data, _UTC
+from earthquake.app.earthquake import plot_map, retrieve_data, _UTC, plot_distance_time, get_dist_time
 from datetime import datetime as dt
 from datetime import date as d
 from .database import *
@@ -95,8 +95,23 @@ async def create_plot(email:EmailStr, plot_info: Plot, db: Session = Depends(get
     plot_map(times, data, plot_info.datatype, lon_limits = plot_info.lon_limits, lat_limits = plot_info.lat_limits, ncols = len(plot_info.dates), clims = plot_info.clims, savefig = savefig)
     return {"message":"success"}
 
-
-    
+@api.post("/create_plot_distance_time/")
+async def create_plot_dt(email: EmailStr, plot_info: PlotDT, db: Session = Depends(get_db)):
+    user = get_user_by_email(db=db, email=email)
+    if not user:
+        logger.error(f"User not found")
+        raise HTTPException(status_code=404, detail="User not found")
+    path = os.path.dirname(plot_info.path)
+    data = retrieve_data(plot_info.path, plot_info.datatype)
+    x, y, c = get_dist_time(data, plot_info.markers)
+    base_name = os.path.basename(plot_info.path)
+    file_name = os.path.splitext(base_name)[0]+"dt"
+    print(file_name)
+    savefig = os.path.join(path, file_name).replace('\\', '/')
+    print(savefig)
+    create_result_db(db=db, email=email, filename=file_name, path = savefig)
+    plot_distance_time(x, y, c, plot_info.datatype, clims=plot_info.clims, savefig=savefig)
+    return {"message":"success"}
     
 
 
